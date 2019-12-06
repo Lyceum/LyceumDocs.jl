d59a3ce489d6ad9ad9f527a2d8c118c78a5ad2d2
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-06 08:19:14 +0000

    build based on b482417

---
+++ previews/PR1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Lyceum-1","page":"Home","title":"Lyceum","text":"","category":"section"},{"location":"#Package-Statuses-1","page":"Home","title":"Package Statuses","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"import Main: package_table_markdown_nodocs, LYCEUM_PACKAGE_DEFS\npackage_table_markdown_nodocs(LYCEUM_PACKAGE_DEFS)","category":"page"}]
+}
