b640bc67a7d57f3460669684e0b486aa15c5bfdc
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-03 21:25:22 +0000

    build based on f778a73

---
+++ previews/PR10/static/scripts/examples/MPPI.jl
@@ -0,0 +1,31 @@
+using LinearAlgebra, Random, Statistics # From Stdlib
+using LyceumAI # For the MPPI controller
+using LyceumMuJoCo # For the PointMass environment
+using LyceumBase.Tools # For the ControllerIterator discussed below
+using Plots
+
+exper = Experiment("/tmp/pointmass.jlso", overwrite=true);
+
+env = LyceumMuJoCo.PointMass();
+mppi = MPPI(
+    sharedmemory_envctor = (i)->sharedmemory_envs(LyceumMuJoCo.PointMass, i),
+    covar0 = Diagonal(0.001^2*I, size(actionspace(env), 1)),
+    lambda = 0.005,
+    K =  32,
+    H = 25,
+    gamma = 0.99
+);
+
+iter = ControllerIterator(mppi, env; T=1000, plotiter=100);
+for (t, traj) in iter
+end
+
+plot(iter.trajectory.rewards)
+
+for (k, v) in pairs(iter.trajectory)
+    exper[k] = v
+end
+finish!(exper);
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
