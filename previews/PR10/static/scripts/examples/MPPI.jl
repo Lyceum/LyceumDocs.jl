1e911429410cb8cd1a5ae288bf2313358fdc5158
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-06 22:45:13 +0000

    build based on 6db99a8

--- previews/PR10/static/scripts/examples/MPPI.jl
+++ previews/PR10/static/scripts/examples/MPPI.jl
@@ -4,19 +4,19 @@ using LyceumMuJoCo # For the PointMass environment
 using LyceumBase.Tools # For the ControllerIterator discussed below
 using Plots
 
-exper = Experiment("/tmp/pointmass.jlso", overwrite=true);
+exper = Experiment("/tmp/pointmass.jlso", overwrite = true);
 
 env = LyceumMuJoCo.PointMass();
 mppi = MPPI(
-    sharedmemory_envctor = (i)->sharedmemory_envs(LyceumMuJoCo.PointMass, i),
-    covar0 = Diagonal(0.001^2*I, size(actionspace(env), 1)),
+    sharedmemory_envctor = (i) -> sharedmemory_envs(LyceumMuJoCo.PointMass, i),
+    covar0 = Diagonal(0.001^2 * I, size(actionspace(env), 1)),
     lambda = 0.005,
-    K =  32,
+    K = 32,
     H = 25,
-    gamma = 0.99
+    gamma = 0.99,
 );
 
-iter = ControllerIterator(mppi, env; T=1000, plotiter=100);
+iter = ControllerIterator(mppi, env; T = 1000, plotiter = 100);
 for (t, traj) in iter
 end
 
