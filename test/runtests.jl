7e972423350a6859ce893d961887fab869935256
Colin Summers <colinxs@cs.washington.edu>
2020-01-12 23:09:54 -0800

    fix (#15)
    windows is failing (#17) but everything else looks good. merging.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,38 +1,38 @@
 using Test, Pkg, LyceumDocs
 
+function collect_scripts()
+    scripts = String[]
+    for (root, _, files) in walkdir(LyceumDocs.SRC_DIR), file in files
+        abs_path = joinpath(root, file)
+        if LyceumDocs.isliterate(abs_path, read(abs_path, String))
+            push!(scripts, abs_path)
+        end
+    end
+    scripts
+end
+
+function do_one(script)
+    curdir = pwd()
+    curproj = Base.active_project()
+    tmpdir = mktempdir()
+    try
+        LyceumDocs.create_example_project(tmpdir)
+        cd(tmpdir)
+        Pkg.activate(".")
+        run(`julia --check-bounds=yes --startup-file=no --project=$(tmpdir) $(script)`)
+        return true
+    catch e
+        @error e
+        return false
+    finally
+        cd(curdir)
+        Pkg.activate(curproj)
+        rm(tmpdir, force = true, recursive = true)
+    end
+end
+
 @testset "LyceumDocs.jl" begin
-
-
-    function collect_scripts()
-        scripts = String[]
-        for (root, _, files) in walkdir(LyceumDocs.SRC_DIR), file in files
-            abs_path = joinpath(root, file)
-            if LyceumDocs.isliterate(abs_path, read(abs_path, String))
-                push!(scripts, abs_path)
-            end
-        end
-        scripts
-    end
-
-    function do_one(script)
-        curdir = pwd()
-        curproj = Base.active_project()
-        tmpdir = mktempdir()
-        try
-            LyceumDocs.create_example_project(tmpdir)
-            cd(tmpdir)
-            Pkg.activate(".")
-            return success(`julia --check-bounds=yes --startup-file=no --project $script`)
-        finally
-            cd(curdir)
-            Pkg.activate(curproj)
-            rm(tmpdir, force = true, recursive = true)
-        end
-    end
-
     @testset "$(relpath(script, LyceumDocs.SRC_DIR))" for script in collect_scripts()
         @test do_one(script)
     end
-
-
 end
