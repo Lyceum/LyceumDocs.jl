9026dff0eb74ffccaf6ecf1aaa25b7b93205dc6f
colinxs <me@colinxsummers.com>
2020-01-12 18:36:21 -0800

    format

--- docs/src/examples/NPG.jl
+++ docs/src/examples/NPG.jl
@@ -34,11 +34,18 @@ dobs, dact = length(obsspace(env)), length(actionspace(env));
 # The policy is more than just a feed forward nerual network, however. It's treated as a
 # stochastic variable, and thus we track the log of the standard deviation of noise to apply
 # to the action sampling; this is final zero vector of size 'dact'.
-policy = DiagGaussianPolicy(multilayer_perceptron(dobs, 32, 32, dact;
-                                                  σ=tanh,
-                                                  initb=glorot_uniform,
-                                                  initb_final=glorot_uniform),
-                            zeros(dact))
+policy = DiagGaussianPolicy(
+    multilayer_perceptron(
+        dobs,
+        32,
+        32,
+        dact;
+        σ = tanh,
+        initb = glorot_uniform,
+        initb_final = glorot_uniform,
+    ),
+    zeros(dact),
+)
 policy = Flux.paramtype(Float32, policy); # We make sure the Policy is a consistent type
 
 
@@ -47,24 +54,31 @@ policy = Flux.paramtype(Float32, policy); # We make sure the Policy is a consist
 # of the current policy's performance from an estimate of the value function on the same inputs.
 # The calculation of the advangate is more stable in for gradient descent. We represent the value
 # function as a neural network as well.
-value = multilayer_perceptron(dobs, 128, 128, 1;
-                              σ=Flux.relu,
-                              initb=glorot_uniform,
-                              initb_final=glorot_uniform)
+value = multilayer_perceptron(
+    dobs,
+    128,
+    128,
+    1;
+    σ = Flux.relu,
+    initb = glorot_uniform,
+    initb_final = glorot_uniform,
+)
 value = Flux.paramtype(Float32, value); # Again, consistent type; imporant for performance
 
 
 
 
 # FluxTrainer is an iterator that loops on the Flux object provided.
-# The result at each loop is passed to stopcb below, so you can quit after 
+# The result at each loop is passed to stopcb below, so you can quit after
 # a number of epochs, convergence, or other criteria; here it's capped at two epochs
 # as a lambda function.
 valueloss(bl, X, Y) = Flux.mse(vec(bl(X)), vec(Y))
-valuetrainer = FluxTrainer(optimiser = ADAM(1e-3),
-                           szbatch = 64,
-                           lossfn = valueloss,
-                           stopcb = s -> s.nepochs > 2);
+valuetrainer = FluxTrainer(
+    optimiser = ADAM(1e-3),
+    szbatch = 64,
+    lossfn = valueloss,
+    stopcb = s -> s.nepochs > 2,
+);
 
 
 # The `NaturalPolicyGradient` iterator is a struct that contains relevant data objects
@@ -76,15 +90,17 @@ valuetrainer = FluxTrainer(optimiser = ADAM(1e-3),
 # number of samples 'N', specified.
 # Multi-threading happens to collect the 'N' samples using as many threads as possible, up to
 # a trajectory length of Hmax.
-npg = NaturalPolicyGradient((i)->tconstruct(LyceumMuJoCo.HopperV2, i),
-                            policy,
-                            value,
-                            valuetrainer;
-                            gamma = 0.995,
-                            gaelambda = 0.97,
-                            norm_step_size = 0.05,
-                            Hmax=1000,
-                            N=10000);
+npg = NaturalPolicyGradient(
+    i -> tconstruct(LyceumMuJoCo.HopperV2, i),
+    policy,
+    value,
+    valuetrainer;
+    gamma = 0.995,
+    gaelambda = 0.97,
+    norm_step_size = 0.05,
+    Hmax = 1000,
+    N = 10000,
+);
 
 #md # ## Running Experiments
 # Finally, let's spin on our iterator 200 times, plotting every 20 iterations.
@@ -92,56 +108,63 @@ npg = NaturalPolicyGradient((i)->tconstruct(LyceumMuJoCo.HopperV2, i),
 # manually if needed. We of course wish to track results, so we create an Experiment to
 # which we can save data. We also
 # have useful timing information displayed every 20 iterations to understand CPU performance.
-exper = Experiment("/tmp/hopper_example.jlso", overwrite=true)
+exper = Experiment("/tmp/hopper_example.jlso", overwrite = true)
 lg = ULogger() # walks, talks, and acts like a Julia logger
 for (i, state) in enumerate(npg)
     if i > 200
         ## serialize some stuff and quit
-        exper[:policy]     = npg.policy
-        exper[:value]      = npg.value
-        exper[:etype]      = LyceumMuJoCo.HopperV2
+        exper[:policy] = npg.policy
+        exper[:value] = npg.value
+        exper[:etype] = LyceumMuJoCo.HopperV2
         exper[:meanstates] = state.meanbatch
         exper[:stocstates] = state.stocbatch
         break
     end
 
     ## log everything in `state` except meanbatch and stocbatch
-    push!(lg, :algstate, filter_nt(state, exclude=(:meanbatch, :stocbatch)))
+    push!(lg, :algstate, filter_nt(state, exclude = (:meanbatch, :stocbatch)))
 
     if mod(i, 20) == 0
         x = lg[:algstate]
         ## The following are helper functions for plotting to the terminal.
         ## The first plot renders the 'Eval' function associated with the env.
-        display(expplot(Line(x[:stocterminal_eval], "StocLastE"),
-                        Line(x[:meanterminal_eval], "MeanLastE"),
-                        title="Evaluation Score, Iter=$i", width=60, height=8
-                       ));
+        display(expplot(
+            Line(x[:stocterminal_eval], "StocLastE"),
+            Line(x[:meanterminal_eval], "MeanLastE"),
+            title = "Evaluation Score, Iter=$i",
+            width = 60,
+            height = 8,
+        ))
 
-        display(expplot(Line(x[:stoctraj_reward], "StocR"),
-                        Line(x[:meantraj_reward], "MeanR"),
-                        title="Reward, Iter=$i", width=60, height=8
-                       ));
+        display(expplot(
+            Line(x[:stoctraj_reward], "StocR"),
+            Line(x[:meantraj_reward], "MeanR"),
+            title = "Reward, Iter=$i",
+            width = 60,
+            height = 8,
+        ))
 
         ## The following is timing values for each component of the last iteration.
         ## It's useful to see where the compute is going.
-        println("elapsed_sampled  = ", state.elapsed_sampled); #md 
-        println("elapsed_gradll   = ", state.elapsed_gradll);  #md 
-        println("elapsed_vpg      = ", state.elapsed_vpg);     #md 
-        println("elapsed_cg       = ", state.elapsed_cg);      #md 
-        println("elapsed_valuefit = ", state.elapsed_valuefit);#md 
+        println("elapsed_sampled  = ", state.elapsed_sampled) #md
+        println("elapsed_gradll   = ", state.elapsed_gradll)  #md
+        println("elapsed_vpg      = ", state.elapsed_vpg)     #md
+        println("elapsed_cg       = ", state.elapsed_cg)      #md
+        println("elapsed_valuefit = ", state.elapsed_valuefit)#md
     end
 end
 
 # Let's go ahead and plot the final reward trajectory and see how we did. The two
 # lines is a property of a Policy Gradient method: there is a stochastic policy that takes
 # the actions of the policy and adds noise to explore for better behavior.
-plot!(plot(lg[:algstate][:meantraj_reward], label="Mean Policy", title="HopperV2 Reward"),
-      lg[:algstate][:stoctraj_reward], label="Stochastic Policy")
+plot!(
+    plot(lg[:algstate][:meantraj_reward], label = "Mean Policy", title = "HopperV2 Reward"),
+    lg[:algstate][:stoctraj_reward],
+    label = "Stochastic Policy",
+)
 
 # and save the logged results to the experiment's JLSO
 for (k, v) in get(lg)
     exper[k] = v
 end
 finish!(exper);
-
-
--- docs/src/examples/humanoid.jl
+++ docs/src/examples/humanoid.jl
@@ -34,7 +34,7 @@ end
 Humanoid() = first(tconstruct(Humanoid, 1))
 function tconstruct(::Type{Humanoid}, n::Integer)
     modelpath = joinpath(@__DIR__, "humanoid.xml")
-    Tuple(Humanoid(s) for s in tconstruct(MJSim, n, modelpath, skip=2))
+    Tuple(Humanoid(s) for s in tconstruct(MJSim, n, modelpath, skip = 2))
 end;
 
 
@@ -47,12 +47,36 @@ end;
 # one can use `simulate.cpp` included with a MuJoCo release to do this, if desired, or
 # use LyceumMuJoCoViz as well.
 _getheight(shapedstate::ShapedView, ::Humanoid) = shapedstate.qpos[3]
-const LAYING_QPOS=[-0.164158, 0.0265899, 0.101116, 0.684044, -0.160277,
-                   -0.70823, -0.0693176, -0.1321, 0.0203937, 0.298099,
-                   0.0873523, 0.00634907, 0.117343, -0.0320319, -0.619764,
-                   0.0204114, -0.157038, 0.0512385, 0.115817, -0.0320437,
-                   -0.617078, -0.00153819, 0.13926, -1.01785, -1.57189,
-                   -0.0914509, 0.708539, -1.57187];
+const LAYING_QPOS = [
+    -0.164158,
+    0.0265899,
+    0.101116,
+    0.684044,
+    -0.160277,
+    -0.70823,
+    -0.0693176,
+    -0.1321,
+    0.0203937,
+    0.298099,
+    0.0873523,
+    0.00634907,
+    0.117343,
+    -0.0320319,
+    -0.619764,
+    0.0204114,
+    -0.157038,
+    0.0512385,
+    0.115817,
+    -0.0320437,
+    -0.617078,
+    -0.00153819,
+    0.13926,
+    -1.01785,
+    -1.57189,
+    -0.0914509,
+    0.708539,
+    -1.57187,
+];
 
 #md # ## Lyceum API Simple Setup
 # LyceumBase requires access to the underlying simulator, thus any LyceumMuJoCo environments
@@ -81,10 +105,10 @@ function LyceumMuJoCo.getreward(state, action, obs, env::Humanoid)
 
     reward = 1.0
     if height < target
-        reward -= 2.0*abs(target - height)
+        reward -= 2.0 * abs(target - height)
     end
 
-    reward -= 1e-3*norm(action)^2
+    reward -= 1e-3 * norm(action)^2
 
     return reward
 end
@@ -108,21 +132,21 @@ end
 # Putting the algorithm in a function allows a user to quickly iterate through parameter
 # searching, or using packages such as `Revise` can seemlessly allow for reloading of
 # algorithms in development.
-function hmMPPI(etype=Humanoid; T=200, H=64, K=64)
+function hmMPPI(etype = Humanoid; T = 200, H = 64, K = 64)
     env = etype()
 
     ## The following parameters work well for this get-up tasks, and make work for
     ## other similar tasks, but is not invariant to the model.
     mppi = MPPI(
-                env_tconstructor = i -> tconstruct(etype, i),
-                covar0 = Diagonal(0.05^2*I, size(actionspace(env), 1)),
-                lambda = 0.4,
-                H = H,
-                K = K,
-                gamma = 1.0
-               )
+        env_tconstructor = i -> tconstruct(etype, i),
+        covar0 = Diagonal(0.05^2 * I, size(actionspace(env), 1)),
+        lambda = 0.4,
+        H = H,
+        K = K,
+        gamma = 1.0,
+    )
 
-    iter = ControllerIterator(mppi, env; T=T, plotiter=div(T, 10))
+    iter = ControllerIterator(mppi, env; T = T, plotiter = div(T, 10))
     ## We can time the following loop; if it ends up less than the time the
     ## MuJoCo models integrated forward in, then one could conceivably run this
     ## MPPI MPC controller interactively...
@@ -130,7 +154,7 @@ function hmMPPI(etype=Humanoid; T=200, H=64, K=64)
     end
 
     savepath = "/tmp/opt_humanoid.jlso"
-    exper = Experiment(savepath, overwrite=true)
+    exper = Experiment(savepath, overwrite = true)
     exper[:etype] = etype
 
     for (k, v) in pairs(iter.trajectory)
@@ -148,5 +172,8 @@ m, d = hmMPPI()
 # to track progress as they go. IF one wanted to review the results after training,
 # or prepare plots for presentations, one can load the data from disk instead.
 x = JLSO.load("/tmp/opt_humanoid.jlso") # one can load the results as such
-plot!(plot(d.trajectory.rewards, label="Inst. Reward", title="Humanoid Standup"),
-      d.trajectory.evaluations, label="Evaluation")
+plot!(
+    plot(d.trajectory.rewards, label = "Inst. Reward", title = "Humanoid Standup"),
+    d.trajectory.evaluations,
+    label = "Evaluation",
+)
