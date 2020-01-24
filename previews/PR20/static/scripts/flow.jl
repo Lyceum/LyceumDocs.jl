485a19a9190d4a35ed7dde8821537cacbf8b8a28
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-01-24 01:53:37 +0000

    build based on 86fb7d6

---
+++ previews/PR20/static/scripts/flow.jl
@@ -0,0 +1,8 @@
+#```julia
+#include("myalgo.jl") # reloads the file and any recent changes, ie parameters
+#include("myenv.jl")  # reloads the environment
+#testalgo()
+#```
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ previews/PR20/static/scripts/starting.jl
@@ -0,0 +1,6 @@
+```bash
+[host]:~/ > export MUJOCO_KEY_PATH=/home/$user/.mujoco/key.txt # replace with appropriate directory
+```
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
